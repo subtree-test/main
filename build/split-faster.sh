2517098f5878fc619bfd9d7be1d4eed3e37bc383
daniel bannert <d.bannert@anolilab.de>
2016-01-30 23:20:16 +0100

    first

---
+++ build/split-faster.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+split()
+{
+    SUBDIR=$1
+    SPLIT=$2
+    HEADS=$3
+
+    mkdir -p $SUBDIR;
+
+    pushd $SUBDIR;
+
+    for HEAD in $HEADS
+    do
+
+        mkdir -p $HEAD
+
+        pushd $HEAD
+
+        git subsplit init git@github.com:subtree-test/main.git
+        git subsplit update
+
+        time git subsplit publish --heads="$HEAD" --no-tags "$SPLIT"
+
+        popd
+
+    done
+
+    popd
+}
+
+split adapter-bar   src/Cache/adapter-bar:git@github.com:subtree-test/foobar-bar       "master"
+split adapter-foo   src/Cache/adapter-foo:git@github.com:subtree-test/foobar-foo       "master"
+split foobar-common src/Cache/foobar-common:git@github.com:subtree-test/foobar-common  "master"
