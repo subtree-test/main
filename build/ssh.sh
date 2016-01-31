f963a83ef403ac37edb0ce9d6e41ea950dabe6de
daniel bannert <d.bannert@anolilab.de>
2016-01-31 23:06:11 +0100

    change file name

--- build/ssh.sh
+++ build/ssh.sh
@@ -1,3 +1,4 @@
+
 openssl aes-256-cbc -K $encrypted_efc26720f134_key -iv $encrypted_efc26720f134_iv -in phpcache.enc -out phpcache -d
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 phpcache # this key should have push access
