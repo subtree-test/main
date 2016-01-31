fc8b93a059a11c3f59ceee53708633fedc973e34
daniel bannert <d.bannert@anolilab.de>
2016-01-31 18:35:44 +0100

    change subsplitt script

--- build/split-faster.sh
+++ build/split-faster.sh
@@ -1,33 +1,7 @@
-#!/bin/bash
 
-split()
-{
-    SUBDIR=$1
-    SPLIT=$2
-    HEADS=$3
+git subsplit init git@github.com:subtree-test/main.git
+git subsplit update
 
-    mkdir -p $SUBDIR;
-
-    pushd $SUBDIR;
-
-    for HEAD in $HEADS
-    do
-
-        mkdir -p $HEAD
-
-        pushd $HEAD
-
-        git subsplit update
-
-        time git subsplit publish "$SPLIT" --heads="$HEAD" --no-tags
-
-        popd
-
-    done
-
-    popd
-}
-
-split adapter-bar   src/Cache/adapter-bar:git@github.com:subtree-test/adapter-bar       "master"
-split adapter-foo   src/Cache/adapter-foo:git@github.com:subtree-test/adapter-foo       "master"
-split foobar-common src/Cache/foobar-common:git@github.com:subtree-test/foobar-common  "master"
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-bar.git
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-foo.git
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-common.git
