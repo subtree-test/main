720c9560dbd4d44235e4ef699cd5fa5dad971b5e
daniel bannert <d.bannert@anolilab.de>
2016-01-31 18:39:22 +0100

    remove .git

--- build/split-faster.sh
+++ build/split-faster.sh
@@ -2,6 +2,6 @@
 git subsplit init git@github.com:subtree-test/main.git
 git subsplit update
 
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-bar.git
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-foo.git
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-common.git
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-bar
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-foo
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-common
