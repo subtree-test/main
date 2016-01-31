0449354f65cc1b74c7461fccdbe34db8c1a4f735
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-01-31 18:44:40 +0100

    Fixes

--- build/split-faster.sh
+++ build/split-faster.sh
@@ -2,6 +2,6 @@
 git subsplit init git@github.com:subtree-test/main.git
 git subsplit update
 
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-bar
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-foo
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/foobar-common
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/adapter-bar
+time git subsplit publish --heads="master" --no-tags src/Cache/adapter-foo:git@github.com/subtree-test/adapter-foo
+time git subsplit publish --heads="master" --no-tags src/Cache/foobar-common:git@github.com/subtree-test/foobar-common
