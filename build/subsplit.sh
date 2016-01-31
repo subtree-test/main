e2d772b945d3e8fca245c76ab587ff8afc07e6a5
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-01-31 19:17:02 +0100

    typo

--- build/subsplit.sh
+++ build/subsplit.sh
@@ -7,7 +7,7 @@ cd git-subsplit && sudo sh ./install.sh && cd ..
 git subsplit init https://github.com/subtree-test/main.git
 git subsplit update
 
-FLAGS="--heads='master' --no-tags"
+FLAGS="--heads=master --no-tags"
 # Push the changes to other repositories
 time git subsplit publish src/Cache/adapter-bar:git@github.com:subtree-test/adapter-bar.git $FLAGS
 time git subsplit publish src/Cache/adapter-foo:git@github.com:subtree-test/adapter-foo.git $FLAGS
