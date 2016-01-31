537f6d9ad549446ba1ace79aa92f331910d0636a
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-01-31 19:09:36 +0100

    Added a flags var

--- build/subsplit.sh
+++ build/subsplit.sh
@@ -7,7 +7,8 @@ cd git-subsplit && sudo sh ./install.sh && cd ..
 git subsplit init https://github.com/subtree-test/main.git
 git subsplit update
 
+FLAGS="--heads=\"master\" --no-tags"
 # Push the changes to other repositories
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-bar:git@github.com/subtree-test/adapter-bar.git
-time git subsplit publish --heads="master" --no-tags src/Cache/adapter-foo:git@github.com/subtree-test/adapter-foo.git
-time git subsplit publish --heads="master" --no-tags src/Cache/foobar-common:git@github.com/subtree-test/foobar-common.git
+time git subsplit publish src/Cache/adapter-bar:git@github.com:subtree-test/adapter-bar.git $FLAGS
+time git subsplit publish src/Cache/adapter-foo:git@github.com:subtree-test/adapter-foo.git $FLAGS
+time git subsplit publish src/Cache/foobar-common:git@github.com:subtree-test/foobar-common.git $FLAGS
